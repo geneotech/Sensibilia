6d6e14152763fa28d0baa6915afbe5e07f9fe3b1
geneotech <unknownunreleased@gmail.com>
2014-02-17 03:53:41 +0100

    blur makes a single pass in while true

--- sensibilia/scripts/effects/blur.lua
+++ sensibilia/scripts/effects/blur.lua
@@ -1,43 +1,30 @@
 function hblur_instability_effect()
+	local last_mult = 0
+		
 	while true do
-		local transitions = table_of_tables(randval(10, 20))
 		
-		--if #transitions < 1 then coroutine.yield() end
+		local transition_duration = randval(10, 500)
+		local target_mult = randval(0.1, 1)
 		
+		local my_val_animator = value_animator(last_mult, target_mult, transition_duration)
 		
-		--print (#transitions)
+		my_val_animator:set_exponential()
 		
-		randomize_properties(10, 500, "transition_duration", transitions)
-		randomize_properties(0.1, 1, "target_mult", transitions)
-		
-		--print (table.inspect(transitions))
-		
-		local last_mult = 0
-		for k, v in ipairs(transitions) do
-			--print(last_mult, v.target_mult)
-			local my_val_animator = value_animator(last_mult, v.target_mult, v.transition_duration)
-			my_val_animator:set_exponential()
-		
-			coroutine.wait(v.transition_duration, function()
-				hblur_program:use()
-				
-				last_mult = my_val_animator:get_animated()
-				--print (last_mult)
-				GL.glUniform1f(h_offset_multiplier, last_mult*instability)
-				
-				
-				fullscreen_pass()
-				
-				
-				vblur_program:use()
-				
-				--last_mult = my_val_animator:get_animated()
-				--print (last_mult)
-				GL.glUniform1f(v_offset_multiplier, last_mult*instability)
-				
-				
-				fullscreen_pass()
-			end)
-		end
+		coroutine.wait(transition_duration, function()
+			hblur_program:use()
+			
+			last_mult = my_val_animator:get_animated()
+			--print (last_mult)
+			GL.glUniform1f(h_offset_multiplier, last_mult*instability)
+			
+			
+			fullscreen_pass()
+			
+			
+			vblur_program:use()
+			
+			GL.glUniform1f(v_offset_multiplier, last_mult*instability)
+			fullscreen_pass()
+		end)
 	end
 end	
