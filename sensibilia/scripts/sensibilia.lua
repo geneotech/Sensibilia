9c255c5123691512eb8843db4c99a33b317d3030
geneotech <unknownunreleased@gmail.com>
2014-02-22 09:34:00 +0100

    moved input routine to input.lua

--- sensibilia/scripts/sensibilia.lua
+++ sensibilia/scripts/sensibilia.lua
@@ -182,59 +182,7 @@ instability_decreaser = timer()
 
 loop_only_info = create_scriptable_info {
 	scripted_events = {
-		[scriptable_component.INTENT_MESSAGE] = 
-			function(message)
-				if message.intent == custom_intents.QUIT then
-					input_system.quit_flag = 1
-				elseif message.intent == custom_intents.GRAVITY_CHANGE then
-					changing_gravity = message.state_flag
-					
-					if message.state_flag then
-						player.crosshair.crosshair.sensitivity.y = 0
-						base_crosshair_rotation = world_camera.camera.last_interpolant.rotation
-						target_gravity_rotation = player.body.physics.body:GetAngle() / 0.01745329251994329576923690768489
-					else
-						player.crosshair.crosshair.sensitivity = config_table.sensitivity
-						world_camera.camera.crosshair_follows_interpolant = false
-					end
-					
-					get_self(player.body):set_gravity_shift_state(changing_gravity)
-					
-					for k, v in ipairs(global_npc_table) do
-						v:set_gravity_shift_state(changing_gravity)
-					end
-					
-				elseif message.intent == custom_intents.RESTART then
-						set_world_reloading_script(reloader_script)
-				elseif message.intent == intent_message.AIM then
-					if changing_gravity then
-						local added_angle = message.mouse_rel.y * 0.6
-					
-						target_gravity_rotation = target_gravity_rotation + added_angle
-						
-						player.body.physics.target_angle = target_gravity_rotation
-						
-						for k, v in ipairs(global_npc_table) do
-							v.entity.physics.target_angle = target_gravity_rotation
-						end
-					end
-				elseif message.intent == custom_intents.INSTANT_SLOWDOWN then
-					physics_system.timestep_multiplier = 0.00001
-				elseif message.intent == custom_intents.SPEED_INCREASE then
-					physics_system.timestep_multiplier = physics_system.timestep_multiplier + 0.05
-				elseif message.intent == custom_intents.SPEED_DECREASE then
-					physics_system.timestep_multiplier = physics_system.timestep_multiplier - 0.05
-					
-					if physics_system.timestep_multiplier < 0.01 then
-						physics_system.timestep_multiplier = 0.01
-					end
-				elseif message.intent == custom_intents.MY_INTENT then
-				
-				end
-
-				
-				return false
-			end,
+		[scriptable_component.INTENT_MESSAGE] = main_input_routine,
 				
 		[scriptable_component.LOOP] = function(subject)
 			gravity_angle_offset = player.body.physics.body:GetAngle() / 0.01745329251994329576923690768489
